30568289d8cfa8d02228727129a1e73573315d2c
Franklin Oliveira Da Veiga <aionprofile@hotmail.com>
2014-09-25 21:38:14 -0300

    aquivos do front android

---
+++ src/com/teamReseveOn/reserveon/MainActivity.java
@@ -0,0 +1,52 @@
+package com.teamReseveOn.reserveon;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_main);
+		
+	}
+	
+	@Override
+	public void onStart() {
+		super.onStart();
+	}
+	
+	@Override
+	public void onRestart() {
+		super.onRestart();
+	}
+	
+	@Override 
+	public void onResume() {
+		super.onResume();
+	}
+	
+	@Override
+	public void onPause() {
+		super.onPause();
+	}
+	
+	@Override
+	public void onStop() {
+		super.onStop();
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+	}
+	
+	public void initTablesActivity(View v) {
+		Intent tablesActivity = new Intent(this, TablesActivity.class);
+		startActivity(tablesActivity);
+	}
+}
